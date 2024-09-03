68d1091805dd64d2b4526d1271b2895ad1cd2e90
daniel <palm6991@gmail.com>
2024-09-03 20:55:51 +0200

    logger

--- src/Infrastructure/Logging/Logger.php
+++ src/Infrastructure/Logging/Logger.php
@@ -2,7 +2,7 @@
 
 namespace Daniel\PaymentSystem\Infrastructure\Logging;
 
-class JsonLogger
+class Logger
 {
 // TODO: Тут можно реализовать логгирование транзакций, или настроить мониторинг что бы отслеживать исправность системы
 }
